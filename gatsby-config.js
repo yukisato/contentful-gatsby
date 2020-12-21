d3e7ef3b7cf16c55f490fcffbd9cb81b54bf42fb
Yuki Sato <yuki.sato.dev@gmail.com>
2020-12-21 14:13:09 -0800

    feat(sitemap): add plugin and set it up for generating sitemap.xml

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,7 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Yuki Sato の海外技術放浪記`,
-    description: `主に技術の話と海外生活のことを書いています。`,
-    twitterId: `@yukisato_dev`,
+    siteUrl: `https://yukisato.dev`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -28,13 +26,80 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        name: `YukiSato.Dev`,
+        short_name: `YukiDev`,
+        start_url: `/blog`,
+        background_color: `#ffffff`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/images/favicon-96x96.png`, // This path is relative to the root of the site.
+      },
+    },
+    // For sitemap.xml
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: "/sitemap.xml",
+        exclude: [
+          `/dev-404-page`,
+          `/404`,
+          `/404.html`,
+          `/offline-plugin-app-shell-fallback`,
+        ],
+        createLinkInHead: true, // optional: create a link in the `<head>` of your site
+        query: `
+        {
+          site {
+            siteMetadata {
+              siteUrl
+            }
+          }
+          allSitePage: allContentfulBlog {
+            nodes {
+              title
+              path: slug
+              node_locale
+              updatedAt
+            }
+          }
+          allContentfulTag {
+            nodes {
+              slug
+              node_locale
+            }
+          }
+        }`,
+        serialize: ({ site, allSitePage, allContentfulTag }) => {
+          const siteUrl = site.siteMetadata.siteUrl
+
+          const blogIndices = ["ja", "en"].map(locale => {
+            return {
+              url: `${siteUrl}/${locale}/blog`,
+              changefreq: `daily`,
+              priority: 0.7,
+            }
+          })
+
+          const pages = allSitePage.nodes
+            .filter(node => node.title !== null)
+            .map(({ node_locale, path }) => {
+              return {
+                url: `${siteUrl}/${node_locale}/blog/${path}`,
+                changefreq: `daily`,
+                priority: 0.7,
+              }
+            })
+
+          const tags = allContentfulTag.nodes.map(({ node_locale, slug }) => {
+            return {
+              url: `${siteUrl}/${node_locale}/blog/tag/${slug}`,
+              changefreq: `daily`,
+              priority: 0.7,
+            }
+          })
+
+          return blogIndices.concat(pages).concat(tags)
+        },
       },
     },
     // To fetch data from Contentful via GraphQL
